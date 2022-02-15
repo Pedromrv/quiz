bb57f8b3cae8c48a584c83f52634944c1423a364
Pedrop <pedrorodriverde@gmail.com>
2022-02-15 16:58:17 +0100

    example questions JS

---
+++ js/script.js
@@ -0,0 +1,68 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How many time zones are there in Russia?",
+    answer: "11",
+    options: [
+      "13",
+      "12",
+      "11",
+      "1000"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What country has the most islands in the world?",
+    answer: "Sweden",
+    options: [
+      "New Zealand",
+      "France",
+      "Melilla",
+      "Sweden"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the slang name for New York City, used by locals?",
+    answer: "Gotham",
+    options: [
+      "Gotham",
+      "Big Apple",
+      "The city",
+      "Algorta"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When did they open the London underground?",
+    answer: "1863",
+    options: [
+      "1992",
+      "1863",
+      "1856",
+      "1968"
+    ]
+  },
+    {
+    numb: 5,
+    question: "When was Netflix founded?",
+    answer: "1997",
+    options: [
+      "2001",
+      "2009",
+      "1997",
+      "2015"
+    ]
+  },
+  //   {
+  //   numb: 6,
+  //   question: "Which of the following empires had no written language: Incan, Aztec, Egyptian, Roman?",
+  //   answer: "Incans",
+  //   options: [
+  //     "Incan",
+  //     "Aztec",
+  //     "Egyptian",
+  //     "Roman"
+  //   ]
+  // },
+];
