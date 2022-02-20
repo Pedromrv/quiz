385293e8ddfe820ddd45f865404b2aa832f75c5f
Pedrop <pedrorodriverde@gmail.com>
2022-02-20 22:33:45 +0100

    last script answers change

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-const respuestas = {
-  rusia: "eleven",
-  island: "suecia",
-  newyork: "gotham",
-  london: "8sixty3",
-  netflix: "ninety7",
-  empire: "inca"
+// let score = 0;
+
+// const respuestas = {
+//   rusia: "eleven",
+//   island: "suecia",
+//   newyork: "gotham",
+//   london: "8sixty3",
+//   netflix: "ninety7",
+//   empire: "inca"
+// }
+
+function getResults() {
+
+  //respuestas correctas
+  var amountCorrect = 0;
+
+// LOOP para hacer check de las preguntas
+  for(var i = 1; i <= 5; i++) {
+    var radiosName = document.getElementsByName('answer'+i);
+
+//LOOP para comprobar las respuestas dentro de cada radio
+    for(var j = 0; j < radiosName.length; j++) {
+      var radiosValue = radiosName[j];
+      if(radiosValue.value == "correct" && radiosValue.checked) {
+        amountCorrect++;
+        radiosValue.style.color = "green";
+      }
+    }
+  }
+document.getElementById('results').innerHTML =
+"Respuestas correctas " + amountCorrect;
+
 }
